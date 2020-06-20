2b74944a6ffa23fb5381941d13397ce231b7ee1d
SyedFaixan <faizan.loas@gmail.com>
2020-06-20 16:49:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  let counter = 5;
   return (
     <div>
-    <p>The value of counter is trhh </p>
-      <p>Update Counter</p>
+    <p>The value of counter is {counter}</p>
+  <button onClick= {()=>{counter++}} name="button">UPDATE COUNTER</button>
     </div>
 
   );
