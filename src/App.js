3934ae731d9eb70e3ec11354b60c56bbc8226b1b
SyedFaixan <faizan.loas@gmail.com>
2020-06-20 16:03:44 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <p>The value of counter is </p>
+      <p>Update Counter</p>
+    </div>
+
+  );
+}
+
+export default App;
