de83b83f1eb0babcdedcfce899f5803b60e585bb
unknown <jamil.mustafa@training4u.it>
2020-06-22 10:34:06 +0200

    changed to html css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import "./App.css";
-
+import React from "react";
+import './mycss.css';
+import MyFile from './Myfile'
 function App() {
-  let [weight, setWeight] = useState(4);
 
   return (
     <div>
-      <p>Weight is: {weight}</p>
-
-      <button
-        onClick={() => {
-          setWeight(weight == 10 ? (weight = 0) : weight + 1);
-        }}
-        name="button"
-      >
-        Update Weight
-      </button>
+      <MyFile></MyFile>
     </div>
   );
 }
