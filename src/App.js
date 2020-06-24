f268d6723543a3b58eae9f0f0ca4cf8bd73cdabc
SyedFaixan <faizan.loas@gmail.com>
2020-06-24 15:37:14 +0500

    facebook-google-login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from "react";
-import "./mycss.css";
-import MyFile from "./Myfile";
+import "./App.css";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
+
+const responseFacebook = (response) => {
+  console.log("login result", response);
+};
+const componentClicked = (data) => {
+  console.warn(data);
+};
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 function App(props) {
   console.log(props);
   return (
     <div>
-      <MyFile appName={props.indexName} appUpdate={"Updates"} />
+      <h1>Login with Facebook</h1>
+      <FacebookLogin
+        appId="256203798781145"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+      />
+      <h1>Login with Google</h1>
+      <GoogleLogin
+        clientId="360526443191-eoaosjlhl752kgpshb8oqcdhjp1l89od.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
